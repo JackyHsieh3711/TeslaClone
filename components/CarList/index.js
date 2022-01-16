7809eb5c8a9dfaf2d12ae4a2afc4326c7c4397b7
JackyHsieh <jackyhsieh1129@gmail.com>
2022-01-16 16:52:54 +0800

    Add CarList component

---
+++ components/CarList/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, FlatList, Dimensions } from 'react-native';
+import CarItem from '../CarItem';
+
+import cars from './cars'
+import styles from './styles'
+
+const CarList = (props) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={cars}
+        renderItem={({ item }) =>
+          <CarItem car={item} />
+        }
+        showsVerticalScrollIndicator={false}
+        snapToAlignment='start'
+        decelerationRate={'fast'}
+        snapToInterval={Dimensions.get('window').height}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
+  )
+}
+
+export default CarList
